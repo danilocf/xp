141eb0ca266d95a1a9a92420cd08a0e6f32a8124
Danilo Freire <danilo.cf.sjc@gmail.com>
2020-04-16 15:24:16 -0300

    add offset

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -6,10 +6,12 @@ import ServiceApi from "../services/ServiceApi";
 class Home extends Component {
   state = {
     loading: true,
+    offset: 0,
+    lastSearch: "",
     search: "",
     searchs: [],
-    albuns: [],
-    musics: [],
+    albums: [],
+    tracks: [],
   };
   render() {
     return (
@@ -29,13 +31,15 @@ class Home extends Component {
         />
         {this.state.search.length ? (
           <React.Fragment>
-            <div className="albuns">
-              <p className="albuns__text style-regular-24-left-light">
+            <div className="albums">
+              <p className="albums__text style-regular-24-left-light">
                 {`Álbuns encontrados para "${this.state.search}"`}
               </p>
-              <div className="albuns__container">
-                {this.state.albuns.length ? (
-                  this.state.albuns.map((item, index) => (
+              <div className="albums__container">
+                {this.state.loading ? (
+                  <p className="style-regular-18-left-grey">Carregando...</p>
+                ) : this.state.albums.length ? (
+                  this.state.albums.map((item, index) => (
                     <Link
                       to={`/album/${item.id}`}
                       className="album"
@@ -54,8 +58,6 @@ class Home extends Component {
                       </p>
                     </Link>
                   ))
-                ) : this.state.loading ? (
-                  <p className="style-regular-18-left-grey">Carregando...</p>
                 ) : (
                   <p className="style-regular-18-left-grey">
                     Nenhum álbum encontrado para "{this.state.search}"
@@ -64,13 +66,15 @@ class Home extends Component {
               </div>
             </div>
 
-            <div className="albuns">
-              <p className="albuns__text style-regular-24-left-light">
+            <div className="albums">
+              <p className="albums__text style-regular-24-left-light">
                 {`Músicas encontradas para "${this.state.search}"`}
               </p>
-              <div className="albuns__container">
-                {this.state.musics.length ? (
-                  this.state.musics.map((item, index) => (
+              <div className="albums__container">
+                {this.state.loading ? (
+                  <p className="style-regular-18-left-grey">Carregando...</p>
+                ) : this.state.tracks.length ? (
+                  this.state.tracks.map((item, index) => (
                     <div className="album" key={index}>
                       <img
                         src={item.album.images[1].url}
@@ -85,8 +89,6 @@ class Home extends Component {
                       </p>
                     </div>
                   ))
-                ) : this.state.loading ? (
-                  <p className="style-regular-18-left-grey">Carregando...</p>
                 ) : (
                   <p className="style-regular-18-left-grey">
                     Nenhum música encontrada para "{this.state.search}"
@@ -96,11 +98,11 @@ class Home extends Component {
             </div>
           </React.Fragment>
         ) : (
-          <div className="albuns">
-            <p className="albuns__text style-regular-24-left-light">
+          <div className="albums">
+            <p className="albums__text style-regular-24-left-light">
               Álbuns buscados recentemente
             </p>
-            <div className="albuns__container">
+            <div className="albums__container">
               {this.state.searchs.length ? (
                 this.state.searchs.map((item, index) => (
                   <Link to={`/album/${item.id}`} className="album" key={index}>
@@ -125,6 +127,12 @@ class Home extends Component {
             </div>
           </div>
         )}
+        <div
+          className="seemore style-bold-36-center-grey"
+          onClick={this.apiSearch}
+        >
+          Ver mais
+        </div>
       </React.Fragment>
     );
   }
@@ -151,10 +159,19 @@ class Home extends Component {
       const { data } = await ServiceApi.search({
         token: this.props.token,
         query: this.state.search,
+        offset: this.state.offset,
       });
       this.setState({
-        albuns: data.albums.items,
-        musics: data.tracks.items,
+        lastSearch: this.state.search,
+        offset: this.state.offset + 5,
+        albums:
+          this.state.search === this.state.lastSearch
+            ? [...this.state.albums, ...data.albums.items]
+            : data.albums.items,
+        tracks:
+          this.state.search === this.state.lastSearch
+            ? [...this.state.tracks, ...data.tracks.items]
+            : data.tracks.items,
       });
     } catch (error) {
       const message = error.response.data.error.message;
